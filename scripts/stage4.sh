1b8e7bf0a84170aa12fbab5a0caa92441753d41d
Palandr123 <palandr545@gmail.com>
2024-05-06 18:59:21 +0300

    ! fix stage4

--- scripts/stage4.sh
+++ scripts/stage4.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
+sed '1d' output/evaluation.csv > output/eval_temp
+sed '1d' output/model1_latitude_predictions.csv > output/m1_lat_pred
+sed '1d' output/model1_longitude_predictions.csv > output/m1_lon_pred
+sed '1d' output/model2_latitude_predictions.csv > output/m2_lat_pred
+sed '1d' output/model2_longitude_predictions.csv > output/m2_lon_pred
+
 hdfs dfs -rm -r project/results
-hdfs dfs -mkdir project/results/evaluation
-hdfs dfs -put /home/team1/project/bigdata-final-project-iu-2024/output/evaluation.csv project/results/evaluation
+hdfs dfs -mkdir project/results
+
+hdfs dfs -put /home/team1/project/bigdata-final-project-iu-2024/output/eval_temp project/results
+hdfs dfs -mv project/results/eval_temp project/results/evaluation.csv
+
+hdfs dfs -put /home/team1/project/bigdata-final-project-iu-2024/output/m1_lat_pred project/results
+hdfs dfs -mv project/results/m1_lat_pred project/results/model1_latitude_predictions.csv
+
+hdfs dfs -put /home/team1/project/bigdata-final-project-iu-2024/output/m1_lon_pred project/results
+hdfs dfs -mv project/results/m1_lon_pred project/results/model1_longitude_predictions.csv
+
+hdfs dfs -put /home/team1/project/bigdata-final-project-iu-2024/output/m2_lat_pred project/results
+hdfs dfs -mv project/results/m2_lat_pred project/results/model2_latitude_predictions.csv
+
+hdfs dfs -put /home/team1/project/bigdata-final-project-iu-2024/output/m2_lon_pred project/results
+hdfs dfs -mv project/results/m2_lon_pred project/results/model2_longitude_predictions.csv
+
+rm output/eval_temp
+rm output/m1_lat_pred
+rm output/m1_lon_pred
+rm output/m2_lat_pred
+rm output/m2_lon_pred
 
 # Get password
 password=$(head -n 1 secrets/.hive.pass)
